2731ec2fba22bc48024e5c7b28c81ced647bfc24
Consoli Tomas <tomicon01@gmail.com>
2022-04-19 17:29:14 -0300

    punto 2 hecho hasta el apartado 3 sin controlar

---
+++ punto2.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Tarea {
+int TareaID; //Numerado en ciclo iterativo
+char *Descripcion; //
+int Duracion; // entre 10 – 100
+} typedef tarea;
+
+int main(){
+    tarea **encargos;
+    tarea **realizadas;
+    int cantEncargos;
+
+    printf("Ingrese la cantidad de tareas que cargara\n");
+    scanf(&cantEncargos);
+    fflush(stdin);
+    encargos= (tarea**)malloc(cantEncargos * sizeof(tarea*));
+
+    for (int i = 0; i < cantEncargos; i++)
+    {
+        encargos[i]= (tarea *)malloc(sizeof(tarea));
+        encargos[i]->Descripcion= (char)malloc(501);
+        printf("Ingrese el id de la tarea");
+        scanf(&(encargos[i]->TareaID));
+        fflush(stdin);
+        printf("Agregue una breve descripcion de la tarea (tiene 500 caracteres)");
+        gets(encargos[i]->Descripcion);
+        do
+        {
+            printf("Que duracion tendra la tarea? (recuerde que debe ingresar un valor entre 10 y 100)");
+            scanf(&(encargos[i]->Duracion));
+            fflush(stdin);
+        } while (encargos[i]->Duracion > 100 || encargos[i]->Duracion < 10);
+    }
+    int confirmar= 0;
+    for (int i = 0; i < cantEncargos; i++)
+    {
+        realizadas[i]= (tarea *)malloc(sizeof(tarea));
+        printf("Si ya se ha realizado la tarea numero %d, ingrese un 1, sino ingrese cualquier otro numero", encargos[i]->TareaID);
+        scanf(&confirmar);
+        if (confirmar == 1)
+        {
+            realizadas[i]= encargos[i];
+            realizadas= NULL;
+            confirmar= 0;
+        }
+        
+    }
+    
+    return 0;
+}
