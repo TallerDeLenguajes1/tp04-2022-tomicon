bce0d7a70c773e1b7a3fc5f5eff4b3475b73c24e
Consoli Tomas <tomicon01@gmail.com>
2022-04-19 17:40:55 -0300

    se corrigieron todos los errores del commit anterior y ahora anda bien

--- punto2.c
+++ punto2.c
@@ -13,23 +13,23 @@ int main(){
     int cantEncargos;
 
     printf("Ingrese la cantidad de tareas que cargara\n");
-    scanf(&cantEncargos);
+    scanf("%d",&cantEncargos);
     fflush(stdin);
     encargos= (tarea**)malloc(cantEncargos * sizeof(tarea*));
 
     for (int i = 0; i < cantEncargos; i++)
     {
         encargos[i]= (tarea *)malloc(sizeof(tarea));
-        encargos[i]->Descripcion= (char)malloc(501);
+        encargos[i]->Descripcion= (char*)malloc(501);
         printf("Ingrese el id de la tarea");
-        scanf(&(encargos[i]->TareaID));
+        scanf("%d",&(encargos[i]->TareaID));
         fflush(stdin);
         printf("Agregue una breve descripcion de la tarea (tiene 500 caracteres)");
         gets(encargos[i]->Descripcion);
         do
         {
             printf("Que duracion tendra la tarea? (recuerde que debe ingresar un valor entre 10 y 100)");
-            scanf(&(encargos[i]->Duracion));
+            scanf("%d",&(encargos[i]->Duracion));
             fflush(stdin);
         } while (encargos[i]->Duracion > 100 || encargos[i]->Duracion < 10);
     }
@@ -38,7 +38,7 @@ int main(){
     {
         realizadas[i]= (tarea *)malloc(sizeof(tarea));
         printf("Si ya se ha realizado la tarea numero %d, ingrese un 1, sino ingrese cualquier otro numero", encargos[i]->TareaID);
-        scanf(&confirmar);
+        scanf("%d",&confirmar);
         if (confirmar == 1)
         {
             realizadas[i]= encargos[i];
